f18dd6770c4a1576d2f8c5736b04f7b963833fc2
Liberontissauri <jp.liberon@gmail.com>
2020-07-03 10:58:08 +0100

    Added a dictionary to access command options

--- Source/Menu.py
+++ Source/Menu.py
@@ -13,15 +13,23 @@ class Menu:
         print("\n"*50 + self.content) # Printing page content
         print((self.terminal_height-3-self.content.count("\n"))*"\n") # Making space to place the prompt at the bottom of the command line
         userinput = prompt(" {} >> ".format(self.selected_option)) # Creating and displaying command prompt
-        if userinput.lower() == "news":
-            self.content = Parser.Page("https://gbatemp.net/","News")
-            self.content = self.content.get()
-            self.selected_option = "News"
+
+        option_dict = {
+            "news":self.news,
+            "exit":self.close
+        }
+        
+        if userinput not in option_dict:
             self.__init__()
-            
-        elif userinput.lower() == "exit":
-            self.close()
+        else:
+            option_dict[userinput]()
     
+    def news(self):
+        self.content = Parser.Page("https://gbatemp.net/","News")
+        self.content = self.content.get()
+        self.selected_option = "News"
+        self.__init__()
+
     def close(self):
         print("\n"*50 + "\033[H\033[J")
     
